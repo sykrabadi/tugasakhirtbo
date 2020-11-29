2a1a6bf3ea5cab368cb48e2e559ee601b55978c7
sykrabadi <muhammad.syukur.1905356@students.um.ac.id>
2020-11-29 21:28:14 +0700

    Additional Methods
    Additional Methods on admin

--- application/views/workout/users_workouts.php
+++ application/views/workout/users_workouts.php
@@ -2,7 +2,8 @@
                 <div class="container-fluid">
                 	<?= $this->session->flashdata('message'); ?>
                     <!-- Page Heading -->
-                    <h1 class="h3 mb-4 text-gray-800"><?= $title;?></h1>
+                    <h1 class="h3 mb-4 text-gray-800"><?= $title;?><span> / <?= $member_detail['name'];?></span></h1>
+                    <h1 class="h3 mb-4 text-gray-800"></h1>
                     <a href="<?= base_url('admin/getallmembers')?>" type="button" class="btn btn-primary mb-3">Kembali</a>
                     <table class="table table-hover">
 						  <thead>
@@ -26,7 +27,7 @@
 						      <td><?= date('d F Y', $mw['tanggal'])?></td>
 						      <td>
 						      <?php if($mw['is_accepted'] == 0): ?>
-						      	<a class="badge badge-warning" href="<?= base_url('admin/setuserworkout/') ?><?= $mw['user_id']?>">Menunggu Konfirmasi</a>
+						      	<a class="badge badge-warning" href="<?= base_url('admin/setuserworkoutbyid/') ?><?= $mw['user_id']?>">Menunggu Konfirmasi</a>
 						      	<?php else: ?>
 						      	<a class="badge badge-success" disabled >Berhasil Dikirim</a>
 						      <?php endif; ?>
